4708ab21c656d8fe9d10956b6363263a86c0cab2
TRANSPORTE-RAUL <gonzarobledo96@gmail.com>
2023-05-04 23:54:31 -0300

    mejorando porfolio

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ document.querySelector('.hamburger').addEventListener('click', (e) => {
   e.currentTarget.classList.toggle('is-active');
 })
 
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-bg");
+  } else {
+    navbar.classList.remove("navbar-bg");
+  }
+});
+
+
 
 
 
